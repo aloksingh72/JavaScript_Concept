517b61d64c8fdc6669580f012921d40fb96b14e0
Alok Singh <aloksingh36757@gmail.com>
2024-05-16 11:24:28 +0530

    conversion

--- conversionOperation.js
+++ conversionOperation.js
@@ -1,5 +1,32 @@
 let someNumber =33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **************************** Operation ****************************
+
+let value =3
+let negValue = -value;
+console.log(negValue);
+
+// // some more operations
+// console.log(2+2);
+// console.log(2*4);
+// console.log(2**2);// use for power of 2
+// console.log(2%3);
+// console.log(4/2);
+
+//note---------> agar phele String aati hai then usko string sbko string ke tarah treat kiya jaayega
+// concatination of string ho jayega
+console.log("1" +2+2);
+
+// but is if number is come first then phele addition hoga then string concatinate hoga
+console.log(1+2 +"3");
+
+
+//prefix and postfix conversion
+// more on mdn prefix and postfix conversion 
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
