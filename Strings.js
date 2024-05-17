e6f5542f6f8f8a84ab876842522a9129f745d196
Alok Singh <aloksingh36757@gmail.com>
2024-05-17 11:57:35 +0530

    new concept

---
+++ Strings.js
@@ -0,0 +1,38 @@
+const name = "aloksingh";
+const repoCount = 150;
+// for string concatenation we use this way
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('alpha-dot-com');
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.indexOf('h'));
+// console.log(gameName.charAt(4));
+// there are mant methods we can study through prototype in browser console.log finction
+// go through these prototypes help to clear the concept of string
+
+const newString = gameName.substring(0, 3);// higher limit include nhi hoti hai 
+console.log(newString);
+
+// slice me hi negative numbers enter kar sakte hai  peeche se slice kam karta hai
+const anotherString = gameName.slice(-4, 4);
+console.log(anotherString);
+
+
+// trim aur replace ke concept 
+// trim  starting and ending blank space ko remove kar dega
+const newStringOne = "   alok singh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+// Replace
+// kisi particular value ko search karke replace kar dega
+const url = "https://aloksingh/github%720singh.com"
+console.log(url.replace('%720', '-'));
+//----------------------^replaceby^^
+
+// spliting on basis of something
+console.log(gameName.split('-'));// kisi bhi cheez ke basis pe split kar sakte hai
+
---
+++ dataTypes.js
@@ -0,0 +1,40 @@
+//-----------------Data Types ke types --------------------------------
+
+// Primitive
+/*
+prmitive dataTypes callByValue hote hai 
+means data copy kar ke diya jata hai exact memory ka refernce nhi diya jata hai
+*/
+// 7 types --> String , Number , Boolean, null ,undefined,Symbol,BigInt
+
+// Non-Primitive 
+// Array,Objects,Functions
+
+
+// *********note -> javascript is dynamically typed language **************** 
+const score =100;
+const scoreValue = 10.5;
+const isLoggesIn = false;
+
+// here symbols is used for unique random values 
+const id = Symbol('123');
+const anotherId = Symbol('123');
+//console.log(id === anotherId);//-->false
+
+//representation ka tareeka 
+// Array
+const heros = ["IronMan","Hulk","CaptainAmerica","Hawkeye"];
+// objects 
+let myInfo={
+    name:"Alok singh",
+    age: 21,
+    mobileNo:8880000000,
+}
+//Functions
+const myFunctions = function(){
+    console.log("Hello World");
+}
+// **********************************************************************
+// memory  in javascript
+// Stack (Primitive ) ,Heap(Non- Primitive)
+
