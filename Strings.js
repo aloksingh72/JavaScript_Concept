4e957755808bcedb812b8b12847609a8d04ed360
Alok Singh <112793361+aloksingh72@users.noreply.github.com>
2024-05-31 21:38:49 +0530

    Update Strings.js

--- Strings.js
+++ Strings.js
@@ -5,7 +5,7 @@ const repoCount = 150;
 // for string concatenation we use this way
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-
+//STRING PROPERTIES 
 const gameName = new String('alpha-dot-com');
 // console.log(gameName[0]);
 // console.log(gameName.length);
